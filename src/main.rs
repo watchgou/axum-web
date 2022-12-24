a683ed497f856ca0dbe66cc02c243b4e7a72363c
jon <zjon234@gmail.com>
2022-12-24 10:00:38 +0800

    2

--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,23 @@ use axum::{
 use std::{marker::PhantomData, net::SocketAddr};
 use tower_http::auth::RequireAuthorizationLayer;
 
-mod jsons;
-
-mod paths;
-
-mod login;
-
-mod token;
-
-
+mod web;
 
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
 
-    let login = Router::new().route("/login", post(login::get_token));
+
+    tracing_subscriber::fmt::init();
+    
+    let login = Router::new().route("/login", post(web::login::get_token));
 
     let app = Router::new()
-        .route("/", get(jsons::root))
-        .route("/users", post(jsons::create_user))
+        .route("/", get(web::jsons::root))
+        .route("/users", post(web::jsons::create_user))
         //  请求参数带有内容
-        .route("/getPath/:id", get(paths::get_user))
-        .route_layer(RequireAuthorizationLayer::custom(token::MyBearer {
+        .route("/getPath/:id", get(web::paths::get_user))
+        .route_layer(RequireAuthorizationLayer::custom(web::token::MyBearer {
             _ty: PhantomData,
         }));
 
