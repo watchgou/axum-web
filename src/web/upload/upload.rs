876148a05226a96d3e1807cea1451a3c1f0ab71e
jon <zjon234@gmail.com>
2022-12-25 11:06:33 +0800

    1

--- src/web/upload/upload.rs
+++ src/web/upload/upload.rs
@@ -2,21 +2,26 @@ use axum::{extract::multipart::Multipart, Json};
 
 use crate::web::Result;
 
-pub async fn upload(mut  multipart: Multipart) -> Json<Result<String>> {
+pub async fn upload(mut multipart: Multipart) -> Json<Result<String>> {
     match multipart.next_field().await {
         Ok(file) => {
             if let Some(field) = file {
                 let name = field.name().unwrap().to_string();
                 //let data = field.bytes().await.unwrap();
                 println!("Length of `{}` is bytes", name);
+                Json(Result::default())
             } else {
-                println!("error");
+                Json(Result {
+                    code: 1,
+                    msg: "file is empty".to_owned(),
+                    data: None,
+                })
             }
         }
-        Err(e) => {
-            println!("{}", e);
-        }
+        Err(_) => Json(Result {
+            code: 1,
+            msg: "file is empty".to_owned(),
+            data: None,
+        }),
     }
-
-    Json(Result::default())
 }
